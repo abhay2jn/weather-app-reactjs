f5766e90f6f16b67a6bf380c7cb7733263016986
abhay <abhayforwork02@gmail.com>
2024-06-11 22:45:59 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios"
+
 
 function App() {
+  //const url = `https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=0282477baad94b774589522ea4511151`
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <div className="container">
+      <div className="top">
+        <div className="location">
+          <p>Delhi</p>
+        </div>
+        <div className="temp">
+          <h1>45°C</h1>
+        </div>
+        <div className="description">
+          <p>Clouds</p>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="feels">
+          <p>50°C</p>
+        </div>
+        <div className="humidity">
+          <p>10%</p>
+        </div>
+        <div className="wind">
+          1 MPH
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
